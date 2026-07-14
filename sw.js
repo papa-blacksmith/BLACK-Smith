9741c1310375e75bd85a80284717d04826dbe493
papa-blacksmith <0911kazu0911@gmail.com>
2026-07-14 18:41:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='black-smith-v02';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
